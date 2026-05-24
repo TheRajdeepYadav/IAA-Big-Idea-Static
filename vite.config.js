c027bf8b98381d4be8c61687c9b38862adea02e0
Rajdeep Yadav <rdzurich@gmail.com>
2026-05-25 00:08:24 +0530

    Update base URL to case-sensitive version '/IAA-Big-Idea-Static/'

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/iaa-big-idea-static/', 
+  base: '/IAA-Big-Idea-Static/', 
 })
